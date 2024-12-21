2f88b8bb730fd3c399d88e4c73bfb8ca494f224e
clarenzmauro <m27oflegend@gmail.com>
2024-12-21 22:53:40 +0800

    Removed old files

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import './App.css';
-import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/Homepage/HomePage';
 import AccountPage from './components/Accounts/AccountPage';
