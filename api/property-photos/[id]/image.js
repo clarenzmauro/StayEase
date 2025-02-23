c836922e90c4cc690abc3f29fe9c0a55753e506c
clarenzmauro <m27oflegend@gmail.com>
2025-02-24 00:51:14 +0800

    Image Test Fix

--- api/property-photos/[id]/image.js
+++ api/property-photos/[id]/image.js
@@ -1,6 +1,13 @@
 import { connectToDatabase } from '../../db';
 import PropertyPhoto from '../../models/PropertyPhoto';
 
+export const config = {
+  api: {
+    bodyParser: false,
+    responseLimit: false
+  }
+};
+
 export default async function handler(req, res) {
   if (req.method !== 'GET') {
     return res.status(405).json({ message: 'Method not allowed' });
@@ -15,9 +22,14 @@ export default async function handler(req, res) {
       return res.status(404).send('Property photo not found');
     }
     
+    // Set appropriate headers
     res.setHeader('Content-Type', 'image/jpeg');
-    res.send(propertyPhoto.photoURL);
+    res.setHeader('Cache-Control', 'public, max-age=31536000, immutable');
+    
+    // Send the binary data
+    res.end(propertyPhoto.photoURL);
   } catch (error) {
+    console.error('Error fetching image:', error);
     res.status(500).json({ message: error.message });
   }
 }
